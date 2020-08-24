20df58d5db0d5831ef730b9197d3a678e0b9303d
Russ <18russjames@gmail.com>
2020-08-24 09:52:00 -0700

    completed assignment

--- App.js
+++ App.js
@@ -1,120 +1,119 @@
-function App(props){
 
 
-//header
-//main
-//section
-//
-/*const introduction = (
-    <div>
-    <section id='introduction'>
-        <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
-    </section>
-    <br />
+function App() {
+
+    const resumeContent =
+    ( <div>
+                <header>
+                    <h1 className="title">About me</h1>
+                </header>
+                <main>
+                    <section id="introduction">
+                        <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
+                    </section>
+                    <br/>
+                        <section id="Education">
+                            <p>I have studied at:</p>
+                            <ul>
+                                <li>IES BEZMILIANA</li>
+                                <li>Minnesota University</li>
+                            </ul>
+                        </section>
+                        <br/>
+                            <section id="works">
+                                <p>I have worked at:</p>
+                                <div id="works_list">
+                                    <ul>
+                                        <li>Github as Engineer during 2018 and there I have to
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </li>
+                                        <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
+                                        A assumenda autem dolorem doloremque
+                    </li>
+                                    </ul>
+                                </div>
+                            </section>
+                            <br/>
+                                <section id="Contact-section">
+                                    <p>My contact information: </p>
+                                    <address id="Contact-info">
+                                        <p>My name is Sally</p>
+                                        <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
+                                        <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
+                                    </address>
+                                </section>
+    </main>
     </div>
-);
+    );
 
-const education = (
-    <div>
-    <section id="Education">
-        <p>I have studied at:</p>
-        <ul>
-            <li>IES BEZMILIANA</li>
-            <li>Minnesota University</li>
-        </ul>
-    </section>
-    <br />
-    </div>
-);
+    return resumeContent;
+};
+
+ReactDOM.render(<App/>, document.getElementById('application-root'));
 
 
-const works = (
-    <div>
-      <section id="works">
-        <p>I have worked at:</p>
-        <div id="works_list">
-            <ul>
-                <li>Github as Engineer during 2018 and there I have to
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                </li>
-                <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
-                    A assumenda autem dolorem doloremque
-                </li>
-            </ul>
+
+    //header
+    //main
+    //section
+    //
+    /*const introduction = (
+        <div>
+        <section id='introduction'>
+            <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
+        </section>
+        <br />
         </div>
-    </section>
-    <br />  
-    </div>
-)
-
-
-const contact =(
-    <div>
-        <section id="Contact-section">
-        <img href="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt='an image of sally' />
-        <p>My contact information: </p>
-        <address id="Contact-info">
-            <p>My name is Sally</p>
-            <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
-            <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
-        </address>
-    </section>
-
-    </div>
-);
-
-return <div><header>{introduction}</header><main>{education}{works}{contact}</main></div>
-*/
-<div>
-<header>
-    <div id="img">
-        
-    </div>
-    <h1 class="title">About me</h1>
-</header>
-
-<main>
-    <section id="introduction">
-        <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
-    </section>
-    <br />
-    <section id="Education">
-        <p>I have studied at:</p>
-        <ul>
-            <li>IES BEZMILIANA</li>
-            <li>Minnesota University</li>
-        </ul>
-    </section>
-    <br />
-    <section id="works">
-        <p>I have worked at:</p>
-        <div id="works_list">
+    );
+    
+    const education = (
+        <div>
+        <section id="Education">
+            <p>I have studied at:</p>
             <ul>
-                <li>Github as Engineer during 2018 and there I have to
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                </li>
-                <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
-                    A assumenda autem dolorem doloremque
-                </li>
+                <li>IES BEZMILIANA</li>
+                <li>Minnesota University</li>
             </ul>
+        </section>
+        <br />
         </div>
-    </section>
-    <br />
-    <section id="Contact-section">
-        <p>My contact information: </p>
-        <address id="Contact-info">
-            <p>My name is Sally</p>
-            <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
-            <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
-        </address>
-    </section>
-</main>
-
-</div>
-
-
-
-
-}
-
-ReactDOM.render(App, document.getElementById('#application-root'))
+    );
+    
+    
+    const works = (
+        <div>
+          <section id="works">
+            <p>I have worked at:</p>
+            <div id="works_list">
+                <ul>
+                    <li>Github as Engineer during 2018 and there I have to
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </li>
+                    <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
+                        A assumenda autem dolorem doloremque
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <br />  
+        </div>
+    )
+    
+    
+    const contact =(
+        <div>
+            <section id="Contact-section">
+            <img href="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt='an image of sally' />
+            <p>My contact information: </p>
+            <address id="Contact-info">
+                <p>My name is Sally</p>
+                <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
+                <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
+            </address>
+        </section>
+    
+        </div>
+    );
+    
+    return <div><header>{introduction}</header><main>{education}{works}{contact}</main></div>
+    */
